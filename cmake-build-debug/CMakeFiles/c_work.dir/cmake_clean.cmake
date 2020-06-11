63fbcdceb6eb8e5f4ffe02b937f9c250a1372716
duanyuxiang <duanyx@belink.com>
2020-06-11 08:49:46 +0800

    First Commit

---
+++ cmake-build-debug/CMakeFiles/c_work.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_work.dir/main.c.obj"
+  "c_work.exe"
+  "c_work.exe.manifest"
+  "c_work.pdb"
+  "libc_work.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_work.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
