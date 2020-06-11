341739ffb79d0bb235118866102c45013c97cc8f
duanyuxiang <duanyx@belink.com>
2020-06-11 08:54:51 +0800

    push

--- cmake-build-debug/CMakeFiles/c_work.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/c_work.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "D:/work_space/c_work/demo1001/main.c" "D:/work_space/c_work/demo1001/cmake-build-debug/CMakeFiles/c_work.dir/main.c.obj"
+  "D:/work_space/c_work/PAT1001/main.c" "D:/work_space/c_work/PAT1001/cmake-build-debug/CMakeFiles/c_work.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/work_space/c_work/demo1001
+# Install script for directory: D:/work_space/c_work/PAT1001
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/work_space/c_work/demo1001/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/work_space/c_work/PAT1001/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
